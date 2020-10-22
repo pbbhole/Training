3e9a478f99c984ab3fd5bd6542c48b6ef5f0f103
Pankaj Bhole <PankajBhole@yahoo.com>
2020-10-22 09:47:18 +0000

    Mult

--- src/main/java/CGIJenkinsBatch/CalculatorAPI/CalculatorAPI.java
+++ src/main/java/CGIJenkinsBatch/CalculatorAPI/CalculatorAPI.java
@@ -11,4 +11,14 @@ public class CalculatorAPI {
 		Res = num1-num2;
 		return Res;
 	}
+	public int Mult(int num1, int num2) {
+		int Res = 0;
+		Res = num1*num2;
+		return Res;
+	}
+	public int Div(int num1, int num2) {
+		int Res = 0;
+		Res = num1/num2;
+		return Res;
+	}
 }
